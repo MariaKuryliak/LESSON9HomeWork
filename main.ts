f4ebbe18602cc81e95cefa7d6f233598ff9453ca
MariaKuryliak <mega_shim@ukr.net>
2020-03-15 22:09:25 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,176 @@
+// Завдання1
+// let city: string;
+// city='Київ';
+// city='Львів';
+// let address: string = city;
+// console.log(address);
+// Завдання2
+
+// function OddEvenNumber(number1:number):number{
+//     let number1 : number = prompt('write number');
+//         return console.log(number1);
+//         //  return (number1%2==0)?
+//         //           console.log('Number is odd')
+//         //         : console.log('Number is even')
+// }
+// let num1: number = OddEvenNumber(5);
+// console.log(num1);
+
+// let num : number = prompt('write number');
+// function OddEvenNumber(num?:number): number{      
+//          return num%2==1 
+//                      ? `Odd`
+//                      : `Even`            
+// }
+// let num1 = OddEvenNumber(num);
+// console.log(num1);
+// function max(...numberArray: number[]){
+//     return 
+// }
+// let x = prompt('write x');
+// let y = prompt('write y');
+
+// function max(x:number, y:number):number{
+//         let max1 : number;
+//          if (x>y){
+//              max1=x;
+//          }
+//          else{
+//              max1=y;
+//          }
+//          return max1;
+// }
+// max(15,11);
+// console.log('max=',max(15,11));
+// var numbers = [45, 4, 9, 16, 55];
+// var over = numbers.filter(myFunction);
+
+
+// function myFunction(value,index,array){
+//         let max : number = 0;
+//         return value>max
+// }
+// console.log(numbers);
+// console.log(over);
+
+// function max(...numberArray:number[]){
+//     let result = numberArray.filter((value,index,array)=>{
+//         var max = 0;
+//         if (value > max) value=max;
+//         return value        
+//     })
+//     console.log(result)
+// }
+// console.log(max(4,5,6,7));
+// let num = prompt('write a number');
+//Завдання 4
+// function getSqrt(num){
+        
+//         if (num>0)
+//              {console.log(Math.sqrt(num))}
+//         else if (num='text')
+//              {console.log('text')}
+//         else if (num<0)
+//              {console.log('negative')}
+//         else if (num='')
+//             {console.log('nothing')}
+
+// }
+// console.log(getSqrt('text'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
